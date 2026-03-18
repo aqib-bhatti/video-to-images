ac68c7724407c1291bc51cdab2766ad50038068d
Muhammad Aqib <saadibhatti1921@gmail.com>
2026-03-18 14:18:59 +0500

    vercel error

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const videoRoutes = require('../src/routes/video.routes');
-const setupDatabase = require('../src/db/setup');
+const videoRoutes = require('./src/routes/video.routes');
+const setupDatabase = require('./src/db/setup');
 
 // Setup database on start
 setupDatabase();
 
 // Start the worker
-require('../src/workers/video.worker');
+require('./src/workers/video.worker');
 
 const app = express();
 const port = 3000;
@@ -26,11 +26,11 @@ app.use((req, res, next) => {
 });
 
 // Serve frontend files from the root 'public' directory
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Fallback route to serve index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.use('/api/v1/video', videoRoutes);
